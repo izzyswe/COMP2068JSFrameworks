d11f38b6c29d84830e9f1dd815d7247da3db8234
izzyswe <ikj134@yahoo.com>
2025-09-22 17:59:36 -0400

    fixed condition, shortened repetitive condition, fixed unneccessary conversion of random value

--- LAB02/lab2.js
+++ LAB02/lab2.js
@@ -8,55 +8,34 @@ function rangeBetween(number, min, max) {
 
 prompt.get(["RPS"], (err, res) => {
 
-  const RPS_CPU = Math.random().toFixed(2)
+  const RPS_CPU = Math.round(Math.random() * 100) / 100  
   const RPS_PAPER = rangeBetween(RPS_CPU, 0.00, 0.34)
   const RPS_SCISSORS = rangeBetween(RPS_CPU, 0.35, 0.67)
   const RPS_ROCK = rangeBetween(RPS_CPU, 0.68, 1.00)
 
   console.log(`\nTest answer: ${RPS_CPU}\ntest paper: ${RPS_PAPER}\ntest-scissors: ${RPS_SCISSORS}\ntest-rock: ${RPS_ROCK} \n\n`) 
 
-  if(RPS_CPU === RPS_PAPER){ 
-    switch (res.RPS) { 
-      case "rock": 
-        console.log(`user: ${res.RPS} \n CPU: Paper \n You Lose!`); 
-        break; 
-      case "paper": 
-        console.log(`user: ${res.RPS} \n CPU: Paper \n Tie!`); 
-        break; 
-      case "scissors": 
-        console.log(`user: ${res.RPS} \n CPU: Paper \n You Win!`); 
-        break; 
-      default: 
-        console.log("Try again"); 
-    } 
-  } else if(RPS_CPU == RPS_SCISSORS){ 
-    switch (res.RPS) { 
-      case "rock": 
-        console.log(`user: ${res.RPS} \n CPU: Paper \n You Lose!`); 
-        break; 
-      case "paper": 
-        console.log(`user: ${res.RPS} \n CPU: Paper \n Tie!`); 
-        break; 
-      case "scissors": 
-        console.log(`user: ${res.RPS} \n CPU: Paper \n You Win!`); 
-        break; 
-      default: 
-        console.log("Try again");
-    }
-  } else if(RPS_CPU == RPS_ROCK){ 
-      switch (res.RPS) { 
-        case "rock": 
-          console.log(`user: ${res.RPS} \n CPU: Paper \n You Lose!`); 
-          break; 
-        case "paper": 
-          console.log(`user: ${res.RPS} \n CPU: Paper \n Tie!`); 
-          break; 
-        case "scissors": 
-          console.log(`user: ${res.RPS} \n CPU: Paper \n You Win!`); 
-          break; 
-        default: 
-          console.log("Try again");
-      }
+  switch (res.RPS) {
+    case "rock":
+    case "1":
+      RPS_PAPER ? console.log(`user: ${res.RPS} \nCPU: Paper\n You Lose!`) : 
+      RPS_ROCK ? console.log(`user: ${res.RPS} \nCPU: Rock\n Tied!`) :
+      RPS_SCISSORS ? console.log(`user: ${res.RPS} \nCPU: Scissors \n You Win!`) : "error";
+      break;
+    case "paper":
+    case "2":
+      RPS_PAPER ? console.log(`user: ${res.RPS} \nCPU: Paper\n Tied!`) : 
+      RPS_ROCK ? console.log(`user: ${res.RPS} \nCPU: Rock\n You Win!`) :
+      RPS_SCISSORS ? console.log(`user: ${res.RPS} \nCPU: Scissors \n You Lose!`) : "error";
+      break;
+    case "scissors":
+    case "3":
+      RPS_PAPER ? console.log(`user: ${res.RPS} \nCPU: Paper\n You Win!`) : 
+      RPS_ROCK ? console.log(`user: ${res.RPS} \nCPU: Rock\n You Lose!`) :
+      RPS_SCISSORS ? console.log(`user: ${res.RPS} \nCPU: Scissors \n Tied!`) : "error";
+      break;
+    default:
+      console.log("Try again");
   }
 })
 
