144e5115ee866017fc24aae69268d0375d7eab56
izzyswe <ikj134@yahoo.com>
2025-09-22 17:51:31 -0400

    declared and initialized variables including debug line

--- LAB02/lab2.js
+++ LAB02/lab2.js
@@ -1,8 +1,19 @@
 // all packages are const, because they will never be modified
 const prompt = require("prompt")
-
-//All the neccessary initialization
-const RPS_CPU = Math.random().toFixed(2)
 prompt.start()
 
+function rangeBetween(number, min, max) {
+  return number >= min && number <= max;
+}
+
+prompt.get(["RPS"], (err, res) => {
+
+  const RPS_CPU = Math.random().toFixed(2)
+  const RPS_PAPER = rangeBetween(RPS_CPU, 0.00, 0.34)
+  const RPS_SCISSORS = rangeBetween(RPS_CPU, 0.35, 0.67)
+  const RPS_ROCK = rangeBetween(RPS_CPU, 0.68, 1.00)
+
+  console.log(`\nTest answer: ${RPS_CPU}\ntest paper: ${RPS_PAPER}\ntest-scissors: ${RPS_SCISSORS}\ntest-rock: ${RPS_ROCK} \n\n`) 
+
+})
 
